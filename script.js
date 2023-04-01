b9f5023eee875c67f74a6728b5e9ba7d6da29279
Nathan Andrade <nathan.henriques@ccc.ufcg.edu.br>
2023-04-01 05:26:53 -0300

    envio para whatsapp funcionando

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 let boxAlimento = null;
 let boxBebida = null;
 let boxSobremesa = null;
+let pedidoPronto = false;
+let nameAlimento = null;
+let priceAlimento = null;
+let nameBebida = null;
+let priceBebida = null;
+let nameSobremesa = null;
+let priceSobremesa = null;
+let totalPrice = 0;
 
 function confirmarPedido() {
-  console.log(boxAlimento);
-  console.log(boxBebida);
-  console.log(boxSobremesa);
+  console.log(nameAlimento);
+  console.log(nameBebida);
+  console.log(nameSobremesa);
+  totalPrice = (
+    Number(priceAlimento) +
+    Number(priceBebida) +
+    Number(priceSobremesa)
+  ).toFixed(2);
+  console.log(totalPrice);
   if (boxAlimento !== null && boxBebida !== null && boxSobremesa !== null) {
     const confirmar = document.querySelector("button");
     const confirmarTexto = document.querySelector("button span");
     confirmar.disabled = false;
     confirmar.classList.add("liberado");
     confirmarTexto.innerHTML = "Fechar pedido";
+    pedidoPronto = true;
+  }
+}
+
+function enviarPedido() {
+  if (pedidoPronto) {
+    let str = `Olá, gostaria de fazer o pedido: \n - Prato: ${nameAlimento} \n- Bebida: ${nameBebida} \n- Sobremesa: ${nameSobremesa} \nTotal: R$ ${totalPrice}`;
+    console.log(str);
+    window.open("https://wa.me/5583999999999?text=" + encodeURI(str));
   }
 }
 
@@ -23,6 +46,13 @@ function selecionarAlimento(alimento) {
   }
 
   alimento.classList.add("selecionado");
+
+  nameAlimento = document.querySelector(".prato .selecionado h3").textContent;
+
+  priceAlimento = document
+    .querySelector(".prato .selecionado p span")
+    .textContent.replace(",", ".");
+
   confirmarPedido();
 }
 
@@ -32,8 +62,12 @@ function selecionarBebida(bebida) {
   if (alimentoAnterior !== null) {
     alimentoAnterior.classList.remove("selecionado");
   }
-
   bebida.classList.add("selecionado");
+
+  nameBebida = document.querySelector(".bebida .selecionado h3").textContent;
+  priceBebida = document
+    .querySelector(".bebida .selecionado p span")
+    .textContent.replace(",", ".");
   confirmarPedido();
 }
 function selecionarSobremesa(sobremesa) {
@@ -44,5 +78,12 @@ function selecionarSobremesa(sobremesa) {
   }
 
   sobremesa.classList.add("selecionado");
+
+  nameSobremesa = document.querySelector(
+    ".sobremesa .selecionado h3"
+  ).textContent;
+  priceSobremesa = document
+    .querySelector(".sobremesa .selecionado p span")
+    .textContent.replace(",", ".");
   confirmarPedido();
 }
