09241a2907fbaf3431d5b295310b2ddc58e723b6
Nathan Andrade <nathan.henriques@ccc.ufcg.edu.br>
2023-04-01 03:31:47 -0300

    add borda verde no js

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ function teste() {
   const botao = document.querySelector("button span");
   botao.innerHTML = "Oi";
 }
+
+function selecionarAlimento(alimento) {
+  const alimentoAnterior = document.querySelector(".prato .selecionado");
+  if (alimentoAnterior !== null) {
+    alimentoAnterior.classList.remove("selecionado");
+  }
+
+  console.log(alimento);
+  alimento.classList.add("selecionado");
+}
+
+function selecionarBebida(bebida) {
+  const alimentoAnterior = document.querySelector(".bebida .selecionado");
+  if (alimentoAnterior !== null) {
+    alimentoAnterior.classList.remove("selecionado");
+  }
+
+  console.log(bebida);
+  bebida.classList.add("selecionado");
+}
+function selecionarSobremesa(sobremesa) {
+  const alimentoAnterior = document.querySelector(".sobremesa .selecionado");
+  if (alimentoAnterior !== null) {
+    alimentoAnterior.classList.remove("selecionado");
+  }
+
+  console.log(sobremesa);
+  sobremesa.classList.add("selecionado");
+}
