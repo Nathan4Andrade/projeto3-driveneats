bf5f88b86e3d5c8ebba67b24be9b0af89f130234
Nathan Andrade <nathan.henriques@ccc.ufcg.edu.br>
2023-04-01 04:30:20 -0300

    add style of 'confirmar pedido'

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-function teste() {
-  const botao = document.querySelector("button span");
-  botao.innerHTML = "Oi";
+let boxAlimento = null;
+let boxBebida = null;
+let boxSobremesa = null;
+
+function confirmarPedido() {
+  console.log(boxAlimento);
+  console.log(boxBebida);
+  console.log(boxSobremesa);
+  if (boxAlimento !== null && boxBebida !== null && boxSobremesa !== null) {
+    const confirmar = document.querySelector("button");
+    const confirmarTexto = document.querySelector("button span");
+    confirmar.disabled = false;
+    confirmar.classList.add("liberado");
+    confirmarTexto.innerHTML = "Fechar pedido";
+  }
 }
 
 function selecionarAlimento(alimento) {
+  boxAlimento = alimento.innerHTML;
   const alimentoAnterior = document.querySelector(".prato .selecionado");
   if (alimentoAnterior !== null) {
     alimentoAnterior.classList.remove("selecionado");
   }
 
-  console.log(alimento);
   alimento.classList.add("selecionado");
+  confirmarPedido();
 }
 
 function selecionarBebida(bebida) {
+  boxBebida = bebida.innerHTML;
   const alimentoAnterior = document.querySelector(".bebida .selecionado");
   if (alimentoAnterior !== null) {
     alimentoAnterior.classList.remove("selecionado");
   }
 
-  console.log(bebida);
   bebida.classList.add("selecionado");
+  confirmarPedido();
 }
 function selecionarSobremesa(sobremesa) {
+  boxSobremesa = sobremesa.innerHTML;
   const alimentoAnterior = document.querySelector(".sobremesa .selecionado");
   if (alimentoAnterior !== null) {
     alimentoAnterior.classList.remove("selecionado");
   }
 
-  console.log(sobremesa);
   sobremesa.classList.add("selecionado");
+  confirmarPedido();
 }
